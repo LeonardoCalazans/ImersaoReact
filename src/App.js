c417f2a9f1f6b4451562e3982c01139e880ba205
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2020-07-29 10:27:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from './Components/Menu';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      {/* <h1>Hello world</h1>
+      Alguma coisa ai no meio
+      Update automagico ( hot reload não é hot roll)
+      JSX -> [J]ava[S]cript [X]ML */}
+    </div>
+  );
+}
+
+export default App;
